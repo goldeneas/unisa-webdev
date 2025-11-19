15663fc7209f2b6251de5037cfe3b241561a83a1
nicola <nicola.picarella6@gmail.com>
2025-11-19 22:30:37 +0100

    added more db functions

--- src/scripts/db_groups.php
+++ src/scripts/db_groups.php
@@ -25,4 +25,27 @@ function get_groups_starting_with($db, $str) {
     return fetch($res);
 }
 
+function get_group_id_by_name($db, $name) {
+    $sql = "SELECT id FROM groups WHERE name = $1";
+    $res = pg_query_params($db, $sql, array($name));
+
+    return fetch($res);
+}
+
+function add_user_to_group($db, $group_name, $user_email) {
+    $group_id = get_group_id_by_name($db, $group_name);
+    $user_id = get_user_id_by_email($db, $user_email);
+
+    $sql = "INSERT INTO group_participants(group_id, user_id) VALUES($1, $2)";
+    pg_query_params($db, $sql, array($group_id, $user_id));
+}
+
+function get_users_in_group($db, $group_name) {
+    $group_id = get_group_id_by_name($group_name);
+    $sql = "SELECT user_id FROM group_participants WHERE group_id = $1"; 
+    
+    $res = pg_query_params($db, $sql, array($group_id));
+    return fetch($res);
+}
+
 ?>
