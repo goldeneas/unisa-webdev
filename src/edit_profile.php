5ac8ef4e36ec32760558ed2ca47b163799565638
Nicola <nicola.picarella6@gmail.com>
2025-11-21 21:43:40 +0100

    readded comments

--- src/edit_profile.php
+++ src/edit_profile.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-/* if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) { */
-/*     header("Location: login.php"); */
-/*     exit; */
-/* } */
+if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
+    header("Location: login.php");
+    exit;
+}
 
 require_once "scripts/db_connection.php";
 require_once "scripts/db_users.php";
