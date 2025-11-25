d0dff55d9dbe514fa2b15131564c18e771948c89
Nicola <nicola.picarella6@gmail.com>
2025-11-25 19:40:05 +0100

    fixed group_preview css

--- src/group_preview.php
+++ src/group_preview.php
@@ -41,7 +41,7 @@ $dati_gruppo = [
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <?php
