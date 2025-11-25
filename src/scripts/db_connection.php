f4f3d134c625d04ba51de6ede16c7dd540e9d2d5
nicola <nicola.picarella6@gmail.com>
2025-11-25 12:26:19 +0100

    fix typo in db_connection

--- src/scripts/db_connection.php
+++ src/scripts/db_connection.php
@@ -10,7 +10,7 @@
     $password = '1919'; 
 
     $connection_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
-    $db = pg_connect($connection_string) or die('Impossibile connetersi');
+    $db = pg_connect($connection_string) or die('Impossibile connettersi');
 
     create_users_table($db);
     create_groups_table($db);
