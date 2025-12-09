850c2634f37073d6ea66e7909d3b63be0f180ca7
15M-I <matteoiandiorio15@gmail.com>
2025-12-09 14:26:13 +0100

    da tag non semantici a quelli semantici

--- src/centered_banner.php
+++ src/centered_banner.php
@@ -2,14 +2,14 @@
 function spawn_centered_banner($title, $subtitle) {
     printf('
         <body>
-            <div id="id">
-                <div id="id-link" class="form-centered">
+            <main id="id">
+                <section id="id-link" class="form-centered">
                     <h3 class="title">%s</h3>
                     <p class="redirect">
                         %s
                     </p>
-                </div>
-            </div>
+                </section>
+            </main>
         </body>
     ', $title, $subtitle);
 }
