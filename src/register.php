eeaedb394618c4d83644b0d91c9117790be9b5dc
Danielix636 <danielemanzo2005@gmail.com>
2026-01-25 17:59:38 +0100

    sticky form rimane i dati inseriti nei rispettivi campi

--- src/register.php
+++ src/register.php
@@ -48,15 +48,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
     <head>
         <title>Registrazione</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
         <link rel="stylesheet" href="register.css">
         <link rel="stylesheet" href="background.css">
         <link rel="stylesheet" href="centered_banner.css">
 
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-        <script src="script_form.js" defer></script>
     </head>
 
     <body>
@@ -71,26 +71,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
                 <?php endif; ?>
 
-                <form id="Register" method="post" action="<?php echo $_SERVER["PHP_SELF"] ?>">
+                <form id="Register" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
 
                     <p>
                         <label for="name">Nome</label>
-                        <input type="text" placeholder="Inserisci il tuo nome" id="name" name="name" />
+                        <input type="text" placeholder="Inserisci il tuo nome" id="name" name="name" value="<?php echo htmlspecialchars($name_val); ?>" required/>
                     </p>
 
                     <p>
                         <label for="cognome">Cognome</label>
-                        <input type="text" placeholder="Inserisci il tuo cognome" id="surname" name="surname" />
+                        <input type="text" placeholder="Inserisci il tuo cognome" id="surname" name="surname" value="<?php echo htmlspecialchars($surname_val); ?>" required/>
                     </p>
                     
                     <p>
                         <label for="email">E-mail</label>
-                        <input type="email" placeholder="Inserisci la tua e-mail" id="email" name="email" />
+                        <input type="email" placeholder="Inserisci la tua e-mail" id="email" name="email" value="<?php echo htmlspecialchars($email_val); ?>" required/>
                     </p>
                     
                     <p>
                         <label for="password">Password (minimo 6 caratteri)</label>
-                        <input type="password" placeholder="Inserisci la tua password" id="password" name="password" minlength="6" />
+                        <input type="password" placeholder="Inserisci la tua password" id="password" name="password" minlength="6" required/>
 
                         <button type="button" onclick="togglePassword()" id="toggle-password">
                             mostra
