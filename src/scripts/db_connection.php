128b0f73d30a72d96f43e8310f88e3dbc431166b
Nicola <nicola.picarella6@gmail.com>
2026-01-30 11:27:56 +0100

    feat: db connection update

--- src/scripts/db_connection.php
+++ src/scripts/db_connection.php
@@ -1,17 +1,16 @@
 <?php
-    require_once "db_tables.php";
+require_once "db_tables.php";
 
-    //Informazioni sul db
+//Informazioni sul db
 
-    $host = "localhost";
-    $port = '5432';
-    $dbname = 'postgres';
-    $username = 'www';
-    $password = '1919'; 
+$host = "localhost";
+$port = '5432';
+$dbname = 'gruppo10';
+$username = 'www';
+$password = 'www';
 
-    $connection_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
-    $db = pg_connect($connection_string) or die('Impossibile connettersi');
+$connection_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
+$db = pg_connect($connection_string) or die('Impossibile connettersi');
 
-    create_users_table($db);
-    create_groups_table($db);
-?>
+create_users_table($db);
+create_groups_table($db);
